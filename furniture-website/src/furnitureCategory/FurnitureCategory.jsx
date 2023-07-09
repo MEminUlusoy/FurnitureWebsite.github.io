c216af9d396ababf6e81a7146dc81c3d08954246
Asena Yardımcı <yrdmasee@gmail.com>
2023-07-10 01:24:53 +0300

    temlate oluşturuldu

--- furniture-website/src/furnitureCategory/FurnitureCategory.jsx
+++ furniture-website/src/furnitureCategory/FurnitureCategory.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import "./FurnitureCategory.css";
 
 function FurnitureCategory() {
-  return <div>ASENA Klonladı.</div>;
+  return (
+    <div>
+      <div className="category-container">
+        <div className="category-inner-container">
+          <div className="category-inner-left-container">
+            <div className="left-inner-container">
+              <div className="header-items">
+                <p>Ana Sayfa Köşe Koltuklar</p>
+              </div>
+            </div>
+          </div>
+          <div className="category-inner-right-container">
+            <div className="right-inner-container"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default FurnitureCategory;
