10623271b6f93eddf6305b485db9610e55f63852
Emin Ulusoy <ulusoyeminn@gmail.com>
2023-07-10 01:27:43 +0300

    shopping card oluşturuldu

--- furniture-website/src/furnitereProduct/FurnitureProduct.jsx
+++ furniture-website/src/furnitereProduct/FurnitureProduct.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import "./FurnitureProduct.css";
-
+import "./FurnitureProduct.js"
+import { useState } from 'react';
 function FurnitureProduct() {
+
+  const [show, setShow] = useState(true);
+
+  const handleClick = (event) => {
+      if(event.currentTarget.classList.contains("product-left-hidden-card-item1")){
+        
+        setShow(!show);
+
+      }
+  }
+
+
   return (
     <div>
+      <div className="space-top"></div>
       <div className='product-container'>
         <div className="product-inner-container">
           <div className="product-inner-left-container">
@@ -51,17 +65,29 @@ function FurnitureProduct() {
 
             </div>
             <div className="product-left-hidden-card-container">
-              <div className="product-left-hidden-card-item">
+
+              <div onClick={handleClick} className="product-left-hidden-card-item1">
                 <h1>Malzeme Bilgisi</h1>
-                <i class="fa-solid fa-chevron-right"></i>
+                <i  className="fa-solid fa-chevron-right"></i>
               </div>
-              <div className="product-left-hidden-card-item">
+           
+              {
+                show && <div className="hidden-card-item1"><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui autem odio enim doloribus officia eum culpa alias dolore adipisci ea. Molestias beatae aliquid repellendus asperiores quasi sapiente reprehenderit adipisci dolore ducimus, odio animi? Nesciunt ea facilis voluptates iste reiciendis quod quisquam totam nihil. Minima beatae tempora nobis ullam eligendi saepe ea ducimus animi illo debitis ratione quas modi neque iure qui molestiae dolor dignissimos, aspernatur voluptatibus dolore voluptatem perspiciatis hic sed. Praesentium aut magni, sunt voluptas eligendi molestias repudiandae officia voluptate asperiores possimus, incidunt officiis optio, consequuntur facilis repellat cum. Consequatur rerum voluptas, nulla praesentium tempore iure dolorem ad harum?</p></div> 
+              }  
+             
+                 
+         
+
+             
+              <div className="product-left-hidden-card-item2">
                 <h1>Temizlik ve Bakım</h1>
-                <i class="fa-solid fa-chevron-right"></i>
+                <i className="fa-solid fa-chevron-right"></i>
               </div>
-              <div className="product-left-hidden-card-item">
+
+
+              <div className="product-left-hidden-card-item3">
                 <h1>Aletsiz Kurulum</h1>
-                <i class="fa-solid fa-chevron-right"></i>
+                <i className="fa-solid fa-chevron-right"></i>
               </div>
             </div>
           </div> 
@@ -69,90 +95,107 @@ function FurnitureProduct() {
           <div className="product-inner-right-container">
             <div className="product-right-shopping-container">
 
+              <div className="product-right-shopping-left-container">
 
-              <div className="product-right-description-container">
-                <div className="product-right-name-container">
-                  <p>Klem 280 x 171 Köşe Ahşap Ayak - Kadife</p>
-                </div>
-                <div className="product-right-rating-container">
-                  <div className="product-right-rating-star">
-                    <i class="fa-solid fa-star"></i>
-                    <i class="fa-solid fa-star"></i>
-                    <i class="fa-solid fa-star"></i>
-                    <i class="fa-solid fa-star"></i>
-                    <i class="fa-solid fa-star"></i>
+              </div>
+              <div className="product-right-shopping-right-container">
+                <div className="product-right-description-container">
+                  <div className="product-right-name-container">
+                    <p>Klem 280 x 171 Köşe Ahşap Ayak - Kadife</p>
                   </div>
-                  <div className="product-right-rating-comment">
-                    <p>326 yorum</p>
+                  <div className="product-right-rating-container">
+                    <div className="product-right-rating-star">
+                      <i className="fa-solid fa-star"></i>
+                      <i className="fa-solid fa-star"></i>
+                      <i className="fa-solid fa-star"></i>
+                      <i className="fa-solid fa-star"></i>
+                      <i className="fa-solid fa-star"></i>
+                    </div>
+                    <div className="product-right-rating-comment">
+                      <p>326 yorum</p>
+                    </div>
+                  </div>
+                </div>
+                
+
+                <div className="product-right-clothe-container">
+
+                  <div className="product-right-clothe-name-container">
+                    <p>Kumaş - Çağla Yeşili</p>    
+                  </div>
+                  <div className="product-right-clothe-circle-container">
+                    <ul>
+                      <li style={{backgroundColor: "#AACFBD"}}></li>
+                      <li style={{backgroundColor: "#F2E6D8"}}></li>
+                      <li style={{backgroundColor: "#B34640"}}></li>
+                      <li style={{backgroundColor: "#C7A209"}}></li>
+                      <li style={{backgroundColor: "#3A4012"}}></li>
+                      <li style={{backgroundColor: "#999999"}}></li>
+                      <li style={{backgroundColor: "#6A6561"}}></li>
+                      <li style={{backgroundColor: "#011C40"}}></li>
+                      <li style={{backgroundColor: "#01401C"}}></li>
+                      <li style={{backgroundColor: "#7F151D"}}></li>
+                      <li style={{backgroundColor: "#F2F2F2"}}></li>
+                    </ul>
+                  </div>
+                </div>
+
+
+                <div className="product-right-foot-container">
+                  <div className="product-right-foot-name-container">
+                    <p>Ayak-Açık</p>
+                  </div>
+                  
+                  <div className="product-right-foot-circle-container">
+                    <ul>
+                      <li style={{backgroundColor: "#D9B68B"}}></li>
+                      <li style={{backgroundColor: "#593A28"}}></li>
+                      <li style={{backgroundColor: "#0D0D0D"}}></li>
+                    </ul>
+                  </div>
+                </div>
+                
+                <div className="product-right-cb-container">
+                  <div className="product-right-cost">
+                    <p>22.980 TL</p>
+                  </div>
+                  <div className="product-right-button">
+                    <button>Sepete ekle</button>
+                  </div>
+                </div>
+                
+
+                <div className="product-right-cargo">
+                  <div className="product-right-cargo-time">
+                    <ul>
+                      <li>Tahmini Teslim Aralığı: 8-10 Hafta</li>
+                    </ul>
+                  </div>
+                  <div className="product-right-cargo-icon">
+                    <i className="fa-regular fa-credit-card"></i>
+                    <p>Ödeme Seçenekleri</p>
+                  </div>
+                  <div className="product-right-cargo-icon">
+                    <i className="fa-solid fa-truck"></i>
+                    <p>Ücretsiz Teslimat</p>
+                  </div>
+                  <div className="product-right-cargo-icon">
+                    <i className="fa-solid fa-truck-ramp-box"></i>
+                    <p>30 Gün Sorgusuz İade</p>
                   </div>
                 </div>
               </div>
+
               
-
-              <div className="product-right-clothe-container">
-
-                <div className="product-right-clothe-name-container">
-                  <p>Kumaş - Çağla Yeşili</p>    
-                </div>
-                <div className="product-right-clothe-circle-container">
-                  <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                  </ul>
-                </div>
-              </div>
-
-
-              <div className="product-right-foot-container">
-                <div className="product-right-foot-name-container">
-                
-                </div>
-                
-                <div className="product-right-foot-circle-container">
-                  
-                </div>
-              </div>
-              
-              <div className="product-right-cb-container">
-                <div className="product-right-cost">
-                
-                </div>
-                <div className="product-right-button">
-                  
-                </div>
-              </div>
-              
-
-              <div className="product-right-cargo">
-                <div className="product-right-cargo-time">
-    
-                </div>
-                <div className="product-right-cargo-icon">
-    
-                </div>
-                <div className="product-right-cargo-icon">
-    
-                </div>
-                <div className="product-right-cargo-icon">
-    
-                </div>
-              </div>
 
             </div>
           </div> 
         </div>  
       </div>
+      
     </div>
   );
+
 }
 
 export default FurnitureProduct;
