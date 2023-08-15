c337d9bd0430e8e32af4e17c90c5dad4b5fb9242
IAyseKaya <i.ayse-kaya@hotmail.com>
2023-08-16 01:15:58 +0300

    navbar added.

--- furniture-website/src/App.js
+++ furniture-website/src/App.js
@@ -2,12 +2,14 @@ import {BrowserRouter,Routes,Route} from "react-router-dom";
 import FurnitureMain from "./furnitureMain/FurnitureMain.jsx";
 import FurnitureProduct from "./furnitereProduct/FurnitureProduct.jsx";
 import FurnitureCategory from "./furnitureCategory/FurnitureCategory.jsx";
+import Navbar from "./components/navbar/Navbar.jsx"
 import './App.css';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+      <Navbar/>
         <Routes>
           <Route index element={<FurnitureMain/>}/>
           <Route path="product" element={<FurnitureProduct/>}/>
